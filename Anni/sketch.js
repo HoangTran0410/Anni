7d8bf08044b27ae2aa76c0bd5d230e7339da0696
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2018-09-12 15:02:03 +0700

    Add files via upload

--- Anni/sketch.js
+++ Anni/sketch.js
@@ -1,9 +1,10 @@
-var heart = [];
+var countDown = '';
+var numHearts = 15;
+var hearts = [];
+var images = [];
 var links = ['https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678087-heart-512.png',
            'https://images.vexels.com/media/users/3/144097/isolated/preview/3dedcd235214cdde6b4e171fdbf66c9d-heart-icon-by-vexels.png',
             'https://png.icons8.com/cotton/2x/novel.png'];
-
-var images = [];
 
 function preload(){
   for(var i = 0; i < links.length; i++){
@@ -13,16 +14,28 @@ function preload(){
 
 function setup(){
   createCanvas(windowWidth, windowHeight).position(0, 0);
+  rectMode(CENTER);
+  textAlign(CENTER, CENTER);
+  textFont('Consolas');
+  textSize(50);
   
-  for(var i = 0; i < 15; i++){
+  for(var i = 0; i < numHearts; i++){
     var sizeHeart = random(30, 70);
-    heart[i] = new Thing(random(width), random(height), sizeHeart, sizeHeart, floor(random(links.length)));
+    hearts[i] = new Thing(random(width), random(height), sizeHeart, sizeHeart, floor(random(links.length)));
   }
 }
 
 function draw(){
   clear();
-  for(var h of heart){
+  noStroke();
+
+  fill(0, 50);
+  rect(width / 2, height / 2, width, 70);
+
+  fill(255);
+  text(countDown, width / 2, height / 2);
+  
+  for(var h of hearts){
     showThing(h);
     updateThing(h);
   }
@@ -81,3 +94,35 @@ function showThing(t){
 function windowResized(){ 
   resizeCanvas(windowWidth, windowHeight, true);
 }
+
+
+// ============================================
+var countDownDate = new Date("September 14, 2018 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  // document.getElementById("countDown").innerHTML = days + "d " + hours + "h "
+  // + minutes + "m " + seconds + "s ";
+
+  countDown = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countDown").innerHTML = "EXPIRED";
+  }
+}, 1000);
