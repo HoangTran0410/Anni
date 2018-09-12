479089573b59c9dd96670701502ea437e8af5848
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2018-09-12 11:28:31 +0700

    Add files via upload

---
+++ Anni/sketch.js
@@ -0,0 +1,76 @@
+var heart = [];
+var links = ['https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678087-heart-512.png',
+           'https://images.vexels.com/media/users/3/144097/isolated/preview/3dedcd235214cdde6b4e171fdbf66c9d-heart-icon-by-vexels.png',
+            'https://png.icons8.com/cotton/2x/novel.png'];
+
+var images = [];
+
+function preload(){
+  for(var i = 0; i < links.length; i++){
+    images[i] = loadImage(links[i]);
+  }
+}
+
+function setup(){
+  createCanvas(windowWidth, windowHeight).position(0, 0);
+  
+  for(var i = 0; i < 15; i++){
+    var sizeHeart = random(30, 70);
+    heart[i] = new Thing(random(width), random(height), sizeHeart, sizeHeart, floor(random(links.length)));
+  }
+}
+
+function draw(){
+  clear();
+  for(var h of heart){
+    showThing(h);
+    updateThing(h);
+  }
+}
+
+function mouseDragged() {
+}
+
+function Thing(x, y, w, h, imgIndex){
+  this.img = imgIndex;
+  this.pos = createVector(x, y);
+  this.size = createVector(w, h);
+  this.vel = createVector(0, 0);
+}
+
+function updateThing(t){
+  t.vel.add(random(-.5, .5), random(-.1, 0));
+  t.pos.add(t.vel);
+
+  if(mouseIsPressed){
+    fill(170, 20, 10);
+    ellipse(mouseX, mouseY, width / 10, width / 10);
+    if(p5.Vector.dist(createVector(mouseX, mouseY), t.pos) < width / 20 + t.size.x)
+      t.vel.add(createVector(mouseX - pmouseX, mouseY - pmouseY).mult(0.2));
+  }
+  
+  if(t.pos.y < 0){
+    t.pos.y = height + t.size.y;
+    t.pos.x = width / 2;
+    t.vel = createVector(0, 0);
+  } 
+  if(t.pos.x > width || t.pos.x < 0){
+    t.vel.x *= -0.5;
+    t.vel.y *= -0.1
+    if(t.pos.x > width) t.pos.x = width;
+    else t.pos.x = 0;
+  } 
+}
+
+function showThing(t){
+  if(images[t.img]){
+    push();
+    translate(t.pos.x, t.pos.y);
+    image(images[t.img], -t.size.x / 2, -t.size.y / 2, t.size.x, t.size.y); 
+    pop();
+  }
+}
+
+function windowResized(){ 
+  resizeCanvas(windowWidth, windowHeight, true);
+}
